63bf030248e3b29a5ce7a143fb8d51d22af2473c
mwicks <>
2023-12-16 19:49:28 +0000

    Store slices rather than strings.
    FossilOrigin-Name: 8bcb6e2a58f691ed91455b914ed31d72deb7db1683ea9c365d0431a1d3f7b6b6

--- src/string_matching/changesets.py
+++ src/string_matching/changesets.py
@@ -16,63 +16,111 @@ def escape(s):
 
 
 @dataclass
-class Changeset:
-    prefix: Changeset | LeafChangeset
-    suffix: Changeset | LeafChangeset
-    common: str
-
-    def __str__(self) -> str:
-        return str(self.prefix) + escape(self.common) + str(self.suffix)
-
-
-@dataclass
 class LeafChangeset:
-    original: str
-    modified: str
-    original_position: int
-    modified_position: int
+    original: slice
+    modified: slice
 
-    def __str__(self) -> str:
+    def changes(self):
+        yield self
+
+    def show(self, original, modified) -> str:
         result = ""
-        if self.original:
-            result += f"[red strike]{escape(self.original)}[/]"
+
+        original_slice = original[self.original]
+        modified_slice = modified[self.modified]
+
+        if original_slice:
+            result += f"[red strike]{escape(original_slice)}[/]"
             # result += f"[delete]{escape(self.original)}[/delete]"
-        if self.modified:
-            result += f"[green]{escape(self.modified)}[/]"
-            # result += f"[insert]{escape(self.original)}[/insert]"
+        if modified_slice:
+            result += f"[green]{escape(modified_slice)}[/]"
+        # result += f"[insert]{escape(self.original)}[/insert]"
+        return result
+
+    def __str__(self):
+        result = ""
+        if self.original.stop is None or self.original.stop > self.original.start:
+            result += f"original[{self.original.start}:{self.original.stop}]\n"
+        if self.modified.stop is None or self.modified.stop > self.modified.start:
+            result += f"modified[{self.modified.start}:{self.modified.stop}]\n"
         return result
 
 
+@dataclass
+class Changeset:
+    common_original: slice
+    common_modified: slice
+
+    prefix: Changeset | LeafChangeset
+    suffix: Changeset | LeafChangeset
+
+    def changes(self):
+        for change in self.prefix.changes():
+            yield change
+
+        yield self
+
+        for change in self.suffix.changes():
+            yield change
+
+    def show(self, original, modified):
+        return (
+            self.prefix.show(original, modified)
+            + escape(original[self.common_original])
+            + self.suffix.show(original, modified)
+        )
+
+    def __str__(self):
+        s_original = f"{self.common_original.start}:{self.common_original.stop}"
+        s_modified = f"{self.common_modified.start}:{self.common_modified.stop}"
+        return f"original[{s_original}]/modified[{s_modified}]\n"
+
+
 def compute_changesets(
-    original: str, modified: str, original_position: int = 0, modified_position: int = 0
+    original: str,
+    modified: str,
+    original_slice: slice = slice(0, None),
+    modified_slice: slice = slice(0, None),
 ) -> Changeset | LeafChangeset:
-    original_automaton = build(original)
-    lcs_position_original, lcs_position_modified, length = find_lcs(
-        original_automaton, modified
+    automaton_original = build(original[original_slice])
+
+    common_offset_original, common_offset_modified, common_length = find_lcs(
+        automaton_original, modified[modified_slice]
     )
 
-    if length == 0:
+    if common_length == 0:
         changeset = LeafChangeset(
-            original=original,
-            modified=modified,
-            original_position=original_position,
-            modified_position=modified_position,
+            original=original_slice,
+            modified=modified_slice,
         )
     else:
-        common = original[lcs_position_original : lcs_position_original + length]
+        common_original = slice(
+            original_slice.start + common_offset_original,
+            original_slice.start + common_offset_original + common_length,
+        )
+        common_modified = slice(
+            modified_slice.start + common_offset_modified,
+            modified_slice.start + common_offset_modified + common_length,
+        )
+
         prefix = compute_changesets(
-            original[:lcs_position_original],
-            modified[:lcs_position_modified],
-            original_position=original_position,
-            modified_position=modified_position,
+            original,
+            modified,
+            slice(original_slice.start, common_original.start),
+            slice(modified_slice.start, common_modified.start),
         )
         suffix = compute_changesets(
-            original[lcs_position_original + length :],
-            modified[lcs_position_modified + length :],
-            original_position=original_position + lcs_position_original + length,
-            modified_position=modified_position + lcs_position_modified + length,
+            original,
+            modified,
+            slice(common_original.stop, original_slice.stop),
+            slice(common_modified.stop, modified_slice.stop),
         )
-        changeset = Changeset(prefix=prefix, common=common, suffix=suffix)
+        changeset = Changeset(
+            prefix=prefix,
+            suffix=suffix,
+            common_original=common_original,
+            common_modified=common_modified,
+        )
 
     return changeset
 
@@ -81,10 +129,16 @@ if __name__ == "__main__":
     with open(os.path.join(DATA_DIR, "file1")) as file1, open(
         os.path.join(DATA_DIR, "file2")
     ) as file2:
-        s1 = file1.read()
-        s2 = file2.read()
+        original = file1.read()
+        modified = file2.read()
 
-    changesets = compute_changesets(s1, s2)
+    changesets = compute_changesets(original, modified)
 
-    console.print(str(changesets))
+    for changeset in changesets.changes():
+        print(str(changeset))
+
+    markup = changesets.show(original, modified)
+
+    console.print(markup)
+
     # print(str(changesets))

114aba0469b4a6f58eb869ff334d826ad39149e8
mwicks <>
2023-12-18 12:27:01 +0000

    Add more multiple lcs test cases.
    FossilOrigin-Name: 126dd7387b867b39f524d8a90c8468ef0e872dfccb9812dd24485d31ff481d0c

--- tests/test_multiple_strings.py
+++ tests/test_multiple_strings.py
@@ -9,6 +9,13 @@ from string_matching.multiple_strings import find_lcs, concatenate_strings
 @pytest.mark.parametrize(
     "string_set,expected_start_position,expected_length",
     [
+        ((), 0, 0),
+        (("", ""), 0, 0),
+        (("", "abc"), 0, 0),
+        (("abc", ""), 0, 0),
+        (("abc", "abc"), 0, 3),
+        (("abc", "abcdef"), 0, 3),
+        (("abcdef", "def"), 3, 3),
         (("abc", "xbc", "bcxy"), 1, 2),
         (("abcd", "bcdax", "yzbcd"), 1, 3),
     ],

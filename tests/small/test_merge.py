715ef12590c0c2b17ee07aa113cab36dd8c3c719
mwicks <>
2023-12-19 23:21:20 +0000

    Add more explanation in test comments.
    FossilOrigin-Name: 48d6ddc1239bc39a043a3df699416996d18b6d12c5449db09df002a7c8c1dac4

--- tests/small/test_merge.py
+++ tests/small/test_merge.py
@@ -14,18 +14,22 @@ from string_matching.merge import do_merge
         ("", "a", "a", "a"),
         # Remove everything on both branches
         ("a", "", "", ""),
-        # Insert at beginning and insert at end on different branches.
+        # Insert at beginning and end on different branches.
         ("a", "xa", "ay", "xay"),
-        # Adjacent deletions which result in removing everything.
-        ("ab", "b", "a", ""),
         # Insert at beginning and deletion at end.
         ("ab", "xab", "a", "xa"),
+        # Adjacent deletions which result in removing everything.
+        ("ab", "b", "a", ""),
         # Should this be a conflict case?  It's definitely
         # an edge case, but handle it this way:  Inserting
         # token 'x' on one branch just before a token 'a' that was
         # deleted on the other branch leaves the inerted token
         # 'x' at the position where 'a' would have been.
         ("a", "xa", "", "x"),
+        # A slightly more interpreable variation
+        # Branch 0 says, "insert 'x' after the '.'"
+        # Branch 1 says, "remove "a".  The merge is interpreted
+        # to be "insert 'x' after the '.', then remove 'a'
         (".a", ".xa", ".", ".x"),
         ("abcdefg", "abcxyz", "abcxyz", "abcxyz"),
         ("abcdefghij", "abxyzefghij", "abcdefgpqrij", "abxyzefgpqrij"),

07e92fb987bb9432a175eb104a9e42f33bd63460
mwicks <>
2023-12-19 04:23:13 +0000

    Added initial do_merge(); refactored yields
    FossilOrigin-Name: 91804cf74cc012ad2f0cb43cea2689e475836034984a65f373b368a24c272591

---
+++ tests/small/test_merge.py
@@ -0,0 +1,11 @@
+import pytest
+from string_matching.merge import do_merge
+
+
+# @pytest.mark.xfail
+@pytest.mark.parametrize(
+    ["parent", "branch1", "branch2", "merge"],
+    [("abcdefghij", "abxyzefghij", "abcdefgpqrij", "abxyzefgpqrij")],
+)
+def test_merge(parent, branch1, branch2, merge):
+    assert do_merge(parent, branch1, branch2) == merge

507b3adadecb821549824297a2cbb1871ee05271
mwicks <>
2023-12-30 23:11:49 +0000

    Finish adding bytes test cases in test_changesets.py
    FossilOrigin-Name: e24be4bbbb65aaddf7a0fef689bc7fe3e563a7be42c7405f20f458c550e00f01

--- tests/small/test_changesets.py
+++ tests/small/test_changesets.py
@@ -57,8 +57,10 @@ def test_find_changesets_and_apply_reverse_reproduces_string(s1: str, s2: str):
 # We use "named" tuples for the "diffs" in the sequence so that we can
 # easily which tuples are diffs and which are sequences of tokens.  Make
 # sure the tuples that should be named are named.  For the string case,
-# all tuples should named.  For the token case, it's ambigious, so we
-# check only the string case.  The token case uses exactly the same
+# *all* tuples should named.  When the token sequence is provided as a
+# tuple, only the change tuples would be named.  Since it's difficult to
+# identify the "right" answer in the tuple of tokens case, we check only
+# the string/bytstring cases.  The token case uses exactly the same
 # code, so if the named tuples are in the right place when processing
 # strings, they should also be in the right place when processing
 # tokens.
@@ -66,10 +68,13 @@ def test_find_changesets_and_apply_reverse_reproduces_string(s1: str, s2: str):
 
 @pytest.mark.parametrize(
     ["s1", "s2"],
-    FIND_CHANGESETS_STRING_CASES,
+    [
+        *FIND_CHANGESETS_STRING_CASES,
+        *FIND_CHANGESETS_BYTES_CASES,
+    ],
 )
 def test_all_string_changeset_tuples_are_typed(s1: str, s2: str):
     diffed = diff(s1, s2)
     assert all(
-        isinstance(change, Change) or isinstance(change, str) for change in diffed
+        isinstance(change, Change) or type(change) in (bytes, str) for change in diffed
     )

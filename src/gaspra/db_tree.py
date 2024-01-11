79fe203fc0fa4316d27435862b2e3412b8d0534e
mwicks <>
2024-01-11 01:56:37 +0000

    Move SQL code out of db_tree.py
    FossilOrigin-Name: a687c9d3030b5a8120d1475967933f68b84f9fdd34c164e983b5a4d3f4f5db08

--- src/gaspra/db_tree.py
+++ src/gaspra/db_tree.py
@@ -2,53 +2,26 @@ from typing import Callable
 from collections.abc import Hashable, Sequence
 
 from sqlite3 import Connection
+from gaspra.db_tree_sql import (
+    CHANGE_PARENT,
+    CREATE_GRAPH,
+    GET_BEST_PATH,
+    GET_NODE_METRICS,
+    GET_START_PATH,
+    GRAPH_INDEX1,
+    GRAPH_INDEX2,
+    INSERT,
+    OLD_GET_BEST_PATH,
+    OLD_START_PATH,
+    OLD_UPDATE_METRICS,
+    PATH_TO,
+    SELECT_ANYTHING,
+    UPDATE_METRICS,
+    ADD_CHILDx,
+    REMOVE_IF_CHILDx,
+)
 
 from gaspra.db_connections import connection_factory as default_connection_factory
-
-CREATE_GRAPH = """
-CREATE TABLE graph (
-   tag TEXT PRIMARY KEY,
-   parent TEXT,
-   parent_rid INTEGER,
-   child0_rid INTEGER,
-   child1_rid INTEGER,
-   height INTEGER,
-   size INTEGER,
-   base_version TEXT
-)
-"""
-GRAPH_INDEX1 = """
-CREATE INDEX graph_parent ON graph(parent)
-"""
-GRAPH_INDEX2 = """
-CREATE INDEX graph_parent_rid ON graph(parent_rid)
-"""
-
-INSERT = """
-INSERT INTO graph (tag, base_version, height, size)
-  VALUES (?, ?, 1, 1)
-"""
-
-SELECT = """
-SELECT {selection} FROM graph
-WHERE tag = ?
-"""
-
-PATH_TO = """
-WITH RECURSIVE path AS (
-   SELECT g.tag, g.parent_rid
-   FROM graph g
-   WHERE g.tag = ?
-
-   UNION ALL
-
-   SELECT h.tag, h.parent_rid
-   FROM graph h
-   JOIN path
-     ON path.parent_rid = h.rowid
-)
-SELECT tag FROM path
-"""
 
 
 class DBTree:
@@ -67,12 +40,12 @@ class DBTree:
                 cursor.execute(statement)
 
     def __contains__(self, tag: Hashable):
-        statement = SELECT.format(selection="tag")
+        statement = SELECT_ANYTHING.format(selection="tag")
         with self.connection_factory() as connection:
             return connection.cursor().execute(statement, (tag,)).fetchone() is not None
 
     def base_version(self, tag) -> Hashable:
-        statement = SELECT.format(selection="base_version")
+        statement = SELECT_ANYTHING.format(selection="base_version")
         with self.connection_factory() as connection:
             return connection.cursor().execute(statement, (tag,)).fetchone()[0]
 
@@ -96,29 +69,17 @@ class DBTree:
             return None
 
     def change_parent(self, tag, new_parent):
-        CHANGE = """
-        UPDATE graph SET parent = ?, parent_rid = ?
-        WHERE rowid = ?
-        """
-        ADD_CHILDx = """
-        UPDATE graph SET child{x}_rid = ? WHERE rowid = ?
-        """
-        REMOVE_IF_CHILDx = """
-        UPDATE graph SET child{x}_rid = NULL WHERE rowid = ? AND
-        child{x}_rid = ?
-        """
-
         with self.connection_factory() as connection:
             # Get tag's rowid for use as key in child relationships.
             tag_rid = (
                 connection.cursor()
-                .execute(SELECT.format(selection="rowid"), (tag,))
+                .execute(SELECT_ANYTHING.format(selection="rowid"), (tag,))
                 .fetchone()[0]
             )
 
             original_parent, original_parent_rid = (
                 connection.cursor()
-                .execute(SELECT.format(selection="parent, parent_rid"), (tag,))
+                .execute(SELECT_ANYTHING.format(selection="parent, parent_rid"), (tag,))
                 .fetchone()
             )
 
@@ -132,12 +93,14 @@ class DBTree:
             # The "new" parent *must* exist.
             new_parent_rid = (
                 connection.cursor()
-                .execute(SELECT.format(selection="rowid"), (new_parent,))
+                .execute(SELECT_ANYTHING.format(selection="rowid"), (new_parent,))
                 .fetchone()[0]
             )
 
             # Point tag to new parent
-            connection.cursor().execute(CHANGE, (new_parent, new_parent_rid, tag_rid))
+            connection.cursor().execute(
+                CHANGE_PARENT, (new_parent, new_parent_rid, tag_rid)
+            )
             # Add tag as child of new parent on childx
             # Heads get added on child0, splits get added on
             child = 0 if original_parent is None else 1
@@ -148,205 +111,47 @@ class DBTree:
         self._update_metrics(original_parent, original_parent_rid)
         self._update_metrics(new_parent, new_parent_rid)
 
-    def old_get_split(self, tag: Hashable):
-        SELECT = """
-        SELECT height, rowid 
-        FROM graph
-        WHERE tag = ?
-        """
-
-        QUERY = """
-        WITH best_path AS (
-        SELECT
-            g.tag,
-            g.rowid as graph_rowid,
-            g.height,
-            row_number() over 
-            (partition by g.parent_rid
-            order by height desc, g.rowid desc)   AS priority
-        FROM graph g
-        WHERE g.parent_rid = ?
-        )
-        SELECT tag, graph_rowid, height from best_path
-        WHERE best_path.priority = 1
-        """
-        with self.connection_factory() as connection:
-            cursor = connection.cursor()
-            depth = 1
-            path = [tag]
-            height, rowid = cursor.execute(SELECT, (tag,)).fetchone()
-            while depth < height:
-                tag, rowid, height = cursor.execute(QUERY, (rowid,)).fetchone()
-                path.append(tag)
-                depth += 1
-        return tag, path
-
-    def new_get_split(self, tag: Hashable):
-        SELECT = """
-        SELECT height, rowid 
-        FROM graph
-        WHERE tag = ?
-        """
-
-        QUERY = """
-        WITH current AS (
-            SELECT *
-            FROM graph
-            WHERE rowid = ?
-        ),
-        children AS (
-            SELECT child.*,
-                child.rowid as child_rowid
-            FROM current
-                JOIN graph child ON child.rowid = current.child0_rid
-            UNION ALL
-            SELECT child.*,
-                child.rowid as child_rowid
-            FROM current
-                JOIN graph child ON child.rowid = current.child1_rid
-        ),
-        best_path AS (
-            SELECT c.tag,
-                c.child_rowid,
-                c.height,
-                row_number() over (
-                    order by height desc,
-                        child_rowid desc
-                ) AS priority
-            FROM children c
-        )
-        SELECT tag,
-            child_rowid,
-            height
-        from best_path
-        WHERE best_path.priority = 1
-        """
-        with self.connection_factory() as connection:
-            cursor = connection.cursor()
-            depth = 1
-            path = [tag]
-            height, rowid = cursor.execute(SELECT, (tag,)).fetchone()
-            while depth < height:
-                tag, rowid, height = cursor.execute(QUERY, (rowid,)).fetchone()
-                path.append(tag)
-                depth += 1
-        return tag, path
-
     def get_split(self, tag: Hashable):
         return self.new_get_split(tag)
 
-    def _old_update_metrics(self, tag: str, tag_rid: int):
-        UPDATE = """
-        WITH subtree AS (
-          SELECT
-            g.tag,
-            g.rowid as rid,
-            g.parent_rid,
-            1 + coalesce(metrics.size,0) as size,
-            1 + coalesce(metrics.height,0) as height
-          FROM graph g
-          LEFT JOIN (
-            SELECT
-              sum(size) as size, 
-              max(height) as height,
-              parent_rid
-            FROM graph
-            GROUP BY parent_rid
-          ) metrics
-          ON metrics.parent_rid = g.rowid
-        ),
-        recursive_subtree AS (
-            SELECT 
-                tag,
-                rid,
-                parent_rid,
-                size,
-                height
-            FROM subtree
-            WHERE tag = ?
-
-            UNION ALL
-
-            SELECT 
-                subtree.tag,
-                subtree.rid,
-                subtree.parent_rid,
-                subtree.size,
-                subtree.height
-            FROM subtree
-            JOIN recursive_subtree rst ON subtree.rid = rst.parent_rid
-        )
-        UPDATE graph
-        SET 
-            size = (
-                SELECT size 
-                FROM recursive_subtree 
-                WHERE recursive_subtree.tag = graph.tag
-            ),
-            height = (
-                SELECT height 
-                FROM recursive_subtree 
-                WHERE recursive_subtree.tag = graph.tag
-            )
-        WHERE 
-            tag IN (SELECT tag FROM recursive_subtree);
-
-"""
+    def new_get_split(self, tag: Hashable):
         with self.connection_factory() as connection:
-            connection.execute(UPDATE, (tag,))
-
-    def _new_update_metrics(self, tag: str, tag_rid: int):
-        QUERY = """
-        WITH current AS (
-            SELECT rowid as current_rid,
-                *
-            FROM graph
-            WHERE rowid = ?
-        ),
-        children AS (
-            SELECT current.current_rid,
-                current.parent_rid,
-                child.height,
-                child.size
-            FROM current
-                LEFT JOIN graph child ON child.rowid = current.child0_rid
-            UNION ALL
-            SELECT current.current_rid,
-                current.parent_rid,
-                child.height,
-                child.size
-            FROM current
-                LEFT JOIN graph child ON child.rowid = current.child1_rid
-        ),
-        metrics AS (
-            SELECT current_rid,
-                parent_rid,
-                1 + coalesce(max(height), 0) as height,
-                1 + coalesce(sum(size), 0) as size
-            from children
-            group by current_rid,
-                parent_rid
-        )
-        SELECT current_rid, parent_rid, height,size
-        FROM metrics
-        """
-
-        UPDATE = """
-        UPDATE graph
-        SET
-           height = ?,
-           size = ?
-        WHERE
-           rowid = ?
-        """
-
-        with self.connection_factory() as connection:
-            while tag_rid is not None:
-                _, parent_rid, height, size = connection.execute(
-                    QUERY, (tag_rid,)
-                ).fetchone()
-                connection.execute(UPDATE, (height, size, tag_rid))
-                tag_rid = parent_rid
+            cursor = connection.cursor()
+            depth = 1
+            path = [tag]
+            height, rowid = cursor.execute(GET_START_PATH, (tag,)).fetchone()
+            while depth < height:
+                tag, rowid, height = cursor.execute(GET_BEST_PATH, (rowid,)).fetchone()
+                path.append(tag)
+                depth += 1
+        return tag, path
 
     def _update_metrics(self, tag: str, tag_rid: int):
         self._new_update_metrics(tag, tag_rid)
+
+    def _new_update_metrics(self, tag: str, tag_rid: int):
+        with self.connection_factory() as connection:
+            while tag_rid is not None:
+                _, parent_rid, height, size = connection.execute(
+                    GET_NODE_METRICS, (tag_rid,)
+                ).fetchone()
+                connection.execute(UPDATE_METRICS, (height, size, tag_rid))
+                tag_rid = parent_rid
+
+    def old_get_split(self, tag: Hashable):
+        with self.connection_factory() as connection:
+            cursor = connection.cursor()
+            depth = 1
+            path = [tag]
+            height, rowid = cursor.execute(OLD_START_PATH, (tag,)).fetchone()
+            while depth < height:
+                tag, rowid, height = cursor.execute(
+                    OLD_GET_BEST_PATH, (rowid,)
+                ).fetchone()
+                path.append(tag)
+                depth += 1
+        return tag, path
+
+    def _old_update_metrics(self, tag: str, tag_rid: int):
+        with self.connection_factory() as connection:
+            connection.execute(OLD_UPDATE_METRICS, (tag,))
---
+++ src/gaspra/db_tree_sql.py
@@ -0,0 +1,225 @@
+CREATE_GRAPH = """
+CREATE TABLE graph (
+   tag TEXT PRIMARY KEY,
+   parent TEXT,
+   parent_rid INTEGER,
+   child0_rid INTEGER,
+   child1_rid INTEGER,
+   height INTEGER,
+   size INTEGER,
+   base_version TEXT
+)
+"""
+
+GRAPH_INDEX1 = """
+CREATE INDEX graph_parent ON graph(parent)
+"""
+
+GRAPH_INDEX2 = """
+CREATE INDEX graph_parent_rid ON graph(parent_rid)
+"""
+
+INSERT = """
+INSERT INTO graph (tag, base_version, height, size)
+  VALUES (?, ?, 1, 1)
+"""
+
+SELECT_ANYTHING = """
+SELECT {selection} FROM graph
+WHERE tag = ?
+"""
+
+PATH_TO = """
+WITH RECURSIVE path AS (
+   SELECT g.tag, g.parent_rid
+   FROM graph g
+   WHERE g.tag = ?
+
+   UNION ALL
+
+   SELECT h.tag, h.parent_rid
+   FROM graph h
+   JOIN path
+     ON path.parent_rid = h.rowid
+)
+SELECT tag FROM path
+"""
+
+CHANGE_PARENT = """
+UPDATE graph 
+SET parent = ?, parent_rid = ?
+WHERE rowid = ?
+"""
+
+ADD_CHILDx = """
+UPDATE graph SET child{x}_rid = ? WHERE rowid = ?
+"""
+
+REMOVE_IF_CHILDx = """
+UPDATE graph 
+SET child{x}_rid = NULL
+WHERE rowid = ? AND child{x}_rid = ?
+"""
+
+GET_START_PATH = """
+SELECT height, rowid 
+FROM graph
+WHERE tag = ?
+"""
+
+GET_BEST_PATH = """
+WITH current AS (
+    SELECT *
+    FROM graph
+    WHERE rowid = ?
+),
+children AS (
+    SELECT child.*,
+        child.rowid AS child_rowid
+    FROM current
+        JOIN graph child ON child.rowid = current.child0_rid
+    UNION ALL
+    SELECT child.*,
+        child.rowid AS child_rowid
+    FROM current
+        JOIN graph child ON child.rowid = current.child1_rid
+),
+best_path AS (
+    SELECT c.tag,
+        c.child_rowid,
+        c.height,
+        row_number() over (
+            order by height desc,
+                child_rowid desc
+        ) AS priority
+    FROM children c
+)
+SELECT tag,
+    child_rowid,
+    height
+FROM best_path
+WHERE best_path.priority = 1
+"""
+
+GET_NODE_METRICS = """
+WITH current AS (
+    SELECT rowid as current_rid,
+        *
+    FROM graph
+    WHERE rowid = ?
+),
+children AS (
+    SELECT current.current_rid,
+        current.parent_rid,
+        child.height,
+        child.size
+    FROM current
+        LEFT JOIN graph child ON child.rowid = current.child0_rid
+    UNION ALL
+    SELECT current.current_rid,
+        current.parent_rid,
+        child.height,
+        child.size
+    FROM current
+        LEFT JOIN graph child ON child.rowid = current.child1_rid
+),
+metrics AS (
+    SELECT current_rid,
+        parent_rid,
+        1 + coalesce(max(height), 0) as height,
+        1 + coalesce(sum(size), 0) as size
+    from children
+    group by current_rid,
+        parent_rid
+)
+SELECT current_rid, parent_rid, height,size
+FROM metrics
+"""
+
+UPDATE_METRICS = """
+UPDATE graph
+SET
+    height = ?,
+    size = ?
+WHERE
+    rowid = ?
+"""
+
+OLD_START_PATH = """
+SELECT height, rowid 
+FROM graph
+WHERE tag = ?
+"""
+
+OLD_GET_BEST_PATH = """
+WITH best_path AS (
+SELECT
+    g.tag,
+    g.rowid as graph_rowid,
+    g.height,
+    row_number() over 
+    (partition by g.parent_rid
+    order by height desc, g.rowid desc)   AS priority
+FROM graph g
+WHERE g.parent_rid = ?
+)
+SELECT tag, graph_rowid, height from best_path
+WHERE best_path.priority = 1
+"""
+
+OLD_UPDATE_METRICS = """
+WITH subtree AS (
+    SELECT
+    g.tag,
+    g.rowid as rid,
+    g.parent_rid,
+    1 + coalesce(metrics.size,0) as size,
+    1 + coalesce(metrics.height,0) as height
+    FROM graph g
+    LEFT JOIN (
+    SELECT
+        sum(size) as size, 
+        max(height) as height,
+        parent_rid
+    FROM graph
+    GROUP BY parent_rid
+    ) metrics
+    ON metrics.parent_rid = g.rowid
+),
+recursive_subtree AS (
+    SELECT 
+        tag,
+        rid,
+        parent_rid,
+        size,
+        height
+    FROM subtree
+    WHERE tag = ?
+
+    UNION ALL
+
+    SELECT 
+        subtree.tag,
+        subtree.rid,
+        subtree.parent_rid,
+        subtree.size,
+        subtree.height
+    FROM subtree
+    JOIN recursive_subtree rst ON subtree.rid = rst.parent_rid
+)
+UPDATE graph
+SET 
+    size = (
+        SELECT size 
+        FROM recursive_subtree 
+        WHERE recursive_subtree.tag = graph.tag
+    ),
+    height = (
+        SELECT height 
+        FROM recursive_subtree 
+        WHERE recursive_subtree.tag = graph.tag
+    )
+WHERE 
+    tag IN (SELECT tag FROM recursive_subtree);
+
+"""
